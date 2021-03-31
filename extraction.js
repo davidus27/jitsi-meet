2de6f855806bed8ef250187f6dddf18032586ee1
davidus27 <davidisbest27@gmail.com>
2021-03-31 19:22:57 +0200

    adding extractionHandler class

---
+++ extraction.js
@@ -0,0 +1,11 @@
+/**
+ * Class responsible for extraction handling
+ */
+export class ExtractionHandler {
+    /**
+     * @param {object} configuration - Object containing information about data extraction
+     */
+    constructor(configuration) {
+        this.configuration = configuration;
+    }
+}
