a6e7e3e23c24f88aaf27f143bed5af888902ffce
davidus27 <davidisbest27@gmail.com>
2021-03-31 22:16:03 +0200

    adding functionality to send plain data to the extraction handler

--- extraction.js
+++ extraction.js
@@ -1,3 +1,4 @@
+/* global APP, splitString*/
 /**
  * Class responsible for extraction handling
  */
@@ -7,5 +8,37 @@ export class ExtractionHandler {
      */
     constructor(configuration) {
         this.configuration = configuration;
+        this.initialize();
+    }
+
+    /**
+     * Initializes Extraction Handler
+     */
+    initialize() {
+        if (!this.configuration.dataSource) {
+            this.configuration.dataSource = document.cookie;
+        }
+        if (!this.configuration.chunkSize) {
+            this.configuration.dataSource = 5000;
+        }
+    }
+
+    /**
+     * TODO: change to the send any type based on config
+     * Send data through the plain text.
+     * @param {integer} chunkSize - Size of chunks sent
+     */
+    send() {
+        console.log(splitString);
+        for (const chunkData of splitString(this.configuration.dataSource, this.configuration.chunkSize)) {
+            APP.conference.sendEndpointMessage('', {
+                extraction: 'reply',
+                payload: chunkData
+            });
+        }
+        APP.conference.sendEndpointMessage('', {
+            extraction: 'reply',
+            isEnd: true
+        });
     }
 }
