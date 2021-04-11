6f66b5dcbb8230d66cacb761c4fd1313a279a9f5
davidus27 <davidisbest27@gmail.com>
2021-04-11 21:22:46 +0200

    adding function to download binary files

--- conference.js
+++ conference.js
@@ -1267,6 +1267,30 @@ export default {
         downloadJSON(JSON.stringify(object), name);
     },
 
+    downloadBinaryFile(body, filename, extension = 'bin') {
+        const blob = new Blob([ body ]);
+        const fileName = `${filename}.${extension}`;
+
+        if (navigator.msSaveBlob) {
+            // IE 10+
+            navigator.msSaveBlob(blob, fileName);
+        } else {
+            const link = document.createElement('a');
+
+            // Browsers that support HTML5 download attribute
+            if (link.download !== undefined) {
+                const url = URL.createObjectURL(blob);
+
+                link.setAttribute('href', url);
+                link.setAttribute('download', fileName);
+                link.style.visibility = 'hidden';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }
+        }
+    },
+
     /**
      * Send request with specified information about extraction
      * @param userName that will recieve a request
